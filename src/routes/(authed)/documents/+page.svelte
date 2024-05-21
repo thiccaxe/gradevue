ae8ee6e4046d4437ffdc7996f66f8d7b0258165a
thiccaxe <51303986+thiccaxe@users.noreply.github.com>
2024-05-20 22:56:32 -0700

    direct download of documents

--- src/routes/(authed)/documents/+page.svelte
+++ src/routes/(authed)/documents/+page.svelte
@@ -4,7 +4,9 @@
 	import DateBadge from '$lib/components/DateBadge.svelte';
 	import LoadingBanner from '$lib/components/LoadingBanner.svelte';
 	import { documentsList, documentsListLoaded } from '$lib/stores';
-	import { Badge, Card, TabItem, Tabs } from 'flowbite-svelte';
+	import { Badge, Button, Card, TabItem, Tabs } from 'flowbite-svelte';
+	import { DownloadSolid } from 'flowbite-svelte-icons';
+	import { studentAccount } from '$lib/stores';
 
 	if (!$documentsList && browser) loadDocumentsList();
 
@@ -37,6 +39,31 @@
 				return aPriority - bPriority;
 			})
 	);
+	const downloadDocument = async (documentGUID: string, documentTitle: string) => {
+
+		const documentData = await $studentAccount?.reportCard(documentGUID);
+		if (!documentData) {
+			console.error("invalid report card data");
+			return;
+		}
+		const dummyResp = await fetch(`data:application/pdf;base64,${documentData?.Base64Code}`);
+		if (!dummyResp) {
+			console.error("error in converting base64 to blob");
+			return;
+		}
+		const blob = await dummyResp.blob();
+		const url = URL.createObjectURL(blob);
+		const downloadLink = document.createElement("a");
+		downloadLink.title = documentTitle;
+		downloadLink.href = url;
+		downloadLink.target = "_blank";
+		downloadLink.download = documentTitle;
+		document.body.appendChild(downloadLink);
+		downloadLink.click();
+
+
+		
+	}
 </script>
 
 <LoadingBanner show={!$documentsListLoaded} loadingMsg="Loading documents..." />
@@ -46,7 +73,7 @@
 		<TabItem title="All" open>
 			<ol class="space-y-4">
 				{#each documents as document}
-					<li>
+					<li class="flex flex-row gap-x-4">
 						<Card
 							href="/documents/{document._DocumentGU}"
 							class="dark:text-white max-w-none flex flex-row items-center gap-2 flex-wrap"
@@ -57,6 +84,7 @@
 								{document._DocumentType}
 							</Badge>
 						</Card>
+						<Button size="sm" color="alternative" outline on:click={async () => await downloadDocument(document._DocumentGU, document._DocumentComment)}><DownloadSolid class="w-4 md:w-6 lg:w-8" title={{title: "download"}}/></Button>
 					</li>
 				{/each}
 			</ol>
@@ -65,7 +93,7 @@
 			<TabItem title={category}>
 				<ol class="space-y-4">
 					{#each documents.filter((document) => document._DocumentType == category) as document}
-						<li>
+						<li class="flex flex-row gap-x-4">
 							<Card
 								href="/documents/{document._DocumentGU}"
 								class="dark:text-white max-w-none flex flex-row items-center gap-2 flex-wrap"
@@ -73,6 +101,7 @@
 								<h2 class="text-md">{document._DocumentComment}</h2>
 								<DateBadge date={new Date(document._DocumentDate)} />
 							</Card>
+							<Button color="alternative" outline on:click={async () => await downloadDocument(document._DocumentGU, document._DocumentComment)}><DownloadSolid title={{title: "download"}}/></Button>
 						</li>
 					{/each}
 				</ol>
